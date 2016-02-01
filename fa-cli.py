5c13ee7c9893f6dece38901375c4071fec12dd08
Karan <karansharma1295@gmail.com>
2016-02-01 17:12:39 +0530

    all args added

--- fa-cli.py
+++ fa-cli.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-#Author mr-karan
-
+#Author : mr-karan (Karan Sharma)
 import yaml
 import requests
 import json
@@ -17,6 +16,21 @@ parser.add_argument('-a', '--aliases', action='store_true' ,help='Aliases')
 parser.add_argument('-c', '--category', action='store_true'  ,help='Category Icons')
 args = parser.parse_args()
 
+def give_id_unicode(p_list,*text):
+    if text:
+        id_unicode = {p_list[i][2]:p_list[i][3] for i in range(len(p_list)) if (p_list[i][text[1]] is not None) and (text[0] in p_list[i][text[1]])}
+    else:
+        id_unicode = {p_list[i][2]:p_list[i][3] for i in range(len(p_list))}
+    return id_unicode
+
+def message(give_id,text):
+    return '''Icon Details
+    Name : {0}
+    Unicode : {1}
+    HTML Hex :  {2}
+    Icon :{3}
+
+    '''.format('fa-'+text,give_id[text],'&#x'+give_id[text],html.unescape('&#x'+give_id[text]))
 def clean(a):
     for i in a:
         try:
@@ -48,52 +62,46 @@ except FileNotFoundError:
     with open('.fa_cache','w') as the_file:
         the_file.write(json.dumps(p_list,indent=2))
 
-def auto_complete(p_list):
-    search_by_filter = [p_list[i][1] for i in range(len(p_list)) if (p_list[i][1] is not None)]
-    search_by_aliases = [p_list[i][4] for i in range(len(p_list)) if (p_list[i][4] is not None)]
-    search_by_icon = [p_list[i][2] for i in range(len(p_list)) if (p_list[i][2] is not None)]
-    search_by_category = [p_list[i][0] for i in range(len(p_list)) if (p_list[i][0] is not None)]
-    f_completer = WordCompleter(list(set([k for i in search_by_filter for k in i ])))
-    a_completer = WordCompleter(list(set([k for i in search_by_aliases for k in i ])))
-    i_completer = WordCompleter(search_by_icon)
-    c_completer = WordCompleter(list(set([k.lower() for i in search_by_category for k in i ])))
-    textf = prompt('Search By Filter: ', completer=f_completer)
-    texta = prompt('Search by Aliases: ', completer=a_completer)
+if args.icon:
+    search_by_id=[p_list[i][2] for i in range(len(p_list))]
+    give_id = give_id_unicode(p_list)
+    i_completer = WordCompleter(search_by_id)
     texti = prompt('Enter Icon Name: ', completer=i_completer)
-    textc = prompt('Search by Category: ', completer=c_completer)
-    #print(textf)
-    #print(texta)
-    #print(texti)
-    #print(textc)
-    #html_entity ='&#x'+k['unicode']
-    #print(html_entity)
-    #print(k['name'])
-    #print(html.unescape(html_entity))
-
-#auto_complete(p_list)
-
-#if args.icon:
-#    auto_complete(icon_list)
+    print(message(give_id,texti))
+    
 if args.filter:
     search_by_filter = [p_list[i][1] for i in range(len(p_list)) if (p_list[i][1] is not None)]
     f_completer = WordCompleter(list(set([k for i in search_by_filter for k in i ])))
     textf = prompt('Search By Filter: ', completer=f_completer)
-    give_id = {p_list[i][2]:p_list[i][3] for i in range(len(p_list)) if (p_list[i][1] is not None) and (textf in p_list[i][1])}
+    give_id = give_id_unicode(p_list,textf,1)
     for i in list(give_id.keys()):
         print(i)
     i_completer = WordCompleter(list(give_id.keys()))
     texti = prompt('Enter Icon Name: ', completer=i_completer)
-    msg = '''Icon Details
-    Name : {0}
-    Unicode : {1}
-    HTML Hex :  {2}
-    Icon :{3}
+    print(message(give_id,texti))
 
-    '''.format('fa-'+texti,give_id[texti],'&#x'+give_id[texti],html.unescape('&#x'+give_id[texti]))
-    print(msg)
         
 if args.aliases:
-    auto_complete(p_list)
+    search_by_aliases = [p_list[i][4] for i in range(len(p_list)) if (p_list[i][4] is not None)]
+    f_completer = WordCompleter(list(set([k for i in search_by_aliases for k in i ])))
+    textf = prompt('Search By Aliases: ', completer=f_completer)
+    give_id = give_id_unicode(p_list,textf,4)
+    for i in list(give_id.keys()):
+        print(i)
+    i_completer = WordCompleter(list(give_id.keys()))
+    texti = prompt('Enter Icon Name: ', completer=i_completer)
+    print(message(give_id,texti))
+
 if args.category:
-    auto_complete(p_list)
+    search_by_category = [p_list[i][0] for i in range(len(p_list)) if (p_list[i][0] is not None)]
+    f_completer = WordCompleter(list(set([k for i in search_by_category for k in i ])))
+    textf = prompt('Search By Category: ', completer=f_completer)
+    give_id = give_id_unicode(p_list,textf,0)
+    for i in list(give_id.keys()):
+        print(i)
+    i_completer = WordCompleter(list(give_id.keys()))
+    texti = prompt('Enter Icon Name: ', completer=i_completer)
+    print(message(give_id,texti))
+
+    
     
